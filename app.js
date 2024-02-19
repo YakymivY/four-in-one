eb6552876237d1b1ca093c8e31c3c42d97958682
s1rserg <87946455+s1rserg@users.noreply.github.com>
2024-02-19 18:40:08 +0200

    start of team member page

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 const port = 3000;
 
-app.set('view engine', 'ejs'); // Set the view engine to EJS
-app.set('views', path.join(__dirname, 'public', 'template')); // Set the views directory
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public', 'template'));
 
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', (req, res) => {
+//   res.render('index'); // Render the 'index.ejs' template
+// });
 
 app.get('/', (req, res) => {
-  res.render('index'); // Render the 'index.ejs' template
+  const teamMember = {
+    name: 'John Doe',
+    position: 'Software Developer',
+    description: 'A dedicated team member passionate about coding.',
+    photoUrl: '../images/Mr._Fresh.png',
+  };
+
+  res.render('index', { teamMember });
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
